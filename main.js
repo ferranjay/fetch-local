9c073738023f76c59c1ca46d4f4195977515f30c
ferranjay <ferranjay@gmail.com>
2019-05-28 21:31:57 +0200

    txt and array comments

--- main.js
+++ main.js
@@ -55,3 +55,56 @@ async function catchPalmtree(){
     document.getElementById('palmtree').src = URL.createObjectURL(blob);
 
 }
+
+
+/* HOW TO fetch a txt document 
+
+console.log('fetching a poem');
+      catchPoem()
+        .then(poem => {
+          document.getElementById('poem').innerText = poem;
+          console.log('the file has been fetched');
+        })
+        .catch(error => {
+          console.log('error!');
+          console.error(error);
+        });
+      async function catchPoem() {
+        const response = await fetch('poem.txt');
+        return await response.text();
+      }      
+      
+*/
+
+
+/* HOW TO fetch "multiple" an array of files 
+
+      console.log('fetching multiple files');
+      const filenames = [
+        'images/daniele-levis-pelusi-311027-unsplash.jpg',
+        'images/guy-stevens-746794-unsplash.jpg',
+        'images/sharon-pittaway-98257-unsplash.jpg'
+      ];
+      catchImages(filenames)
+        .then(response => {
+          console.log('it worked');
+        })
+        .catch(error => {
+          console.log('error!');
+          console.error(error);
+        });
+      async function catchImages(filenames) {
+        for (let filename of filenames) {
+          const response = await fetch(filename);
+          const blob = await response.blob();
+          const img = document.createElement('img');
+          img.src = URL.createObjectURL(blob);
+          img.width = '200';
+          document.body.append(img);
+        }
+      }
+
+
+
+*/
+
